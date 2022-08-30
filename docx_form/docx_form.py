7c9f851c4007fbc0431e03aefd2e8c33fdf79519
Reece Bourgeois <reecebourgeois@gmail.com>
2022-08-30 14:22:32 -0500

    formatted with black & added starting point

---
+++ docx_form/docx_form.py
@@ -0,0 +1,13 @@
+class DocxForm:
+    def __init__(self, file_path: str) -> None:
+        self.file_path = file_path
+
+    def print_path(self) -> None:
+        print(self.file_path)
+
+
+# Use this for debugging, then move to a test file.
+# This will run if you run this file directly.
+if __name__ == "__main__":
+    docx_form = DocxForm("bob")
+    docx_form.print_path()
--- tests/docx_form_test.py
+++ tests/docx_form_test.py
@@ -1,8 +1,10 @@
-from docx_form import __version__
+from docx_form import __version__, DocxForm
 
 
 def test_version():
     assert __version__ == "0.1.0"
 
-def test_test():
-    assert True == True
+
+def test_docx_form():
+    docx_form = DocxForm("path")
+    assert docx_form.path == "path"
