b5d8f8fddf1576d4d10c0508f981d785453f311c
Reece Bourgeois <reecebourgeois@gmail.com>
2022-09-01 14:44:55 -0500

    removed unused code

--- docx_form/docx_form.py
+++ docx_form/docx_form.py
@@ -13,13 +13,4 @@ class DocxForm:
 # Use this for debugging, then move to a test file.
 # This will run if you run this file directly.
 if __name__ == "__main__":
-    path: str = "C:\\Users\\reece\\Desktop\\testing.docx"
-    docx_form = DocxForm(path)
-
-    # Unzip the file at path in append mode
-    with ZipFile(path, "a") as docx:
-        root = etree.XML(docx.read("word/document.xml"))
-        
-        # Print to text file at "C:\\Users\\reece\\Desktop\\testing.xml"
-        with open("C:\\Users\\reece\\Desktop\\testing.xml", "w") as f:
-            f.write(etree.tostring(root, pretty_print=True).decode("utf-8"))
+    ...
