b10eca2b639baaef6d6790506b39720229830fb7
rmcclelen2 <54422682+rmcclelen2@users.noreply.github.com>
2022-09-06 15:24:41 -0500

    Dev/file path (#5)
    * file path validation
    
    * Comments for clarity
    
    * Separate Checks into a method
    
    * minor edits

--- docx_form/docx_form.py
+++ docx_form/docx_form.py
@@ -1,13 +1,43 @@
 from zipfile import ZipFile
 from lxml import etree
+import re
 
 
 class DocxForm:
     def __init__(self, file_path: str) -> None:
-        self.file_path = file_path
+
+        self.file_path = self.verify_path(file_path)
 
     def print_path(self) -> None:
         print(self.file_path)
+
+    def verify_path(self, file_path: str):
+        # regex to check for docx extension in file path
+
+        verify = re.compile("\.docx")
+
+        if verify.search(file_path):
+            print("File is docx")
+
+        else:
+            raise Exception("File is not docx")
+
+        # File path verify with file open attempt
+
+        try:
+            open(file_path, "a")
+            print("File Path is valid")
+
+        except OSError:
+            ...
+
+            # Zipfile ops at self.file_path and open xml
+
+        with ZipFile(file_path) as myzip:
+            with myzip.open("word/document.xml") as myfile:
+                myfile.read()
+
+        return file_path
 
 
 # Use this for debugging, then move to a test file.
