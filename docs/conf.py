0a5a6941f0e1a58f0b3ccb954df40e5279acabaf
Reece Bourgeois <reecebourgeois@gmail.com>
2022-09-21 21:26:28 -0500

    Auto-generate documentation from comments (#12)
    * Classes creation  started
    
    * class creation
    
    * All Content Controls are identifiable
    
    * local stuff
    
    * renamed features folder to fix git issue
    
    * Element object is edited, changes are not present in file
    
    * removed local stuff
    
    * bingo
    
    * added sphinx
    
    * changed python requirement
    
    * made note to devs
    
    * moved save and xml wizardry
    
    * removed unused import
    
    * updated for save function
    
    * removed local code
    
    Co-authored-by: Robert Herbert <robert.herbert@selu.edu>
    Co-authored-by: Ryan McClelen <ryan.mcclelen@selu.edu>

---
+++ docs/conf.py
@@ -0,0 +1,36 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(".."))
+
+# To build the docs (Windows):
+# 1. poetry run sphinx-apidoc -o . ..
+# 2. poetry run make.bat html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "docx-form"
+copyright = "2022, Reece Bourgeois, Ryan McClelen, Daniel Howard, and Robert Herbert"
+author = "Reece Bourgeois, Ryan McClelen, Daniel Howard, and Robert Herbert"
+release = "0.1.2"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["sphinx.ext.autodoc"]
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "classic"
+html_static_path = ["_static"]
