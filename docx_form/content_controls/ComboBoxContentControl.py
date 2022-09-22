ce6af2874328e3c11fbe8f5c73393a6a8b22405c
Ryan McClelen <ryan.mcclelen@selu.edu>
2022-09-22 00:03:27 -0500

    Added ComboBox self.type

--- docx_form/content_controls/ComboBoxContentControl.py
+++ docx_form/content_controls/ComboBoxContentControl.py
@@ -16,3 +16,4 @@ class ComboBoxContentControl(DocxContentControl):
 
     def __init__(self, root: Element, file_path: str):
         super().__init__(root, file_path)
+        self.type = "ComboBox Content Control"
