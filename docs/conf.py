faff2e5c8204dc60bee62354ddc72599be8c8b1a
Reece Bourgeois <reecebourgeois@gmail.com>
2022-11-16 16:07:52 -0600

    fix: Updated function call (#53)

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ sys.path.insert(0, os.path.abspath(".."))
 project = "docx-form"
 copyright = "2022, Reece Bourgeois, Ryan McClelen, Daniel Howard, and Robert Herbert"
 author = "Reece Bourgeois, Ryan McClelen, Daniel Howard, and Robert Herbert"
-release = "1.0.1"
+release = "1.0.2"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
